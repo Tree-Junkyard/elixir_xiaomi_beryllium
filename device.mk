48e0f39d14ff8902fb56eece133fb91da7ee41b0
PainKiller3 <ninadpatil100@gmail.com>
2022-11-16 17:00:00 +0000

    beryllium: Update telephony components
    * Update DPM, IMS & RIL system blobs
    * Update telephony props while we're at it.
    * From LA.QSSI.13.0.r1-07400-qssi.0
    
    Change-Id: I6c15cac6c36aaf47651f3082b474b813c31425c4

--- device.mk
+++ device.mk
@@ -300,6 +300,7 @@ PRODUCT_COPY_FILES += \
 
 # Net
 PRODUCT_PACKAGES += \
+    android.system.net.netd@1.1 \
     android.system.net.netd@1.1.vendor
 
 # Neural Network
@@ -332,9 +333,12 @@ PRODUCT_COPY_FILES += \
 
 # Radio
 PRODUCT_PACKAGES += \
-    android.hardware.secure_element@1.0.vendor \
-    android.hardware.radio@1.4.vendor \
-    android.hardware.radio.config@1.2.vendor \
+    android.hardware.secure_element@1.2.vendor \
+    android.hardware.radio@1.6 \
+    android.hardware.radio@1.6.vendor \
+    android.hardware.radio.config@1.3 \
+    android.hardware.radio.config@1.3.vendor \
+    android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
     libjson \
     librmnetctl
