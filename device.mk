17a9dd1ec637987290e6a1f33ca6a67020405937
tweenk <tweenk.pl@gmail.com>
2023-10-06 20:46:36 +0200

    beryllium: Build prebuilt protobuf 3.9.1.
    Needed to work around a Qualcomm HAL prebuilt.
    
    Bug: 203713560
    Bug: 258322017
    Bug: 258589373
    Bug: 258632688
    Test: Forrest
    Change-Id: I8ebc9fdfcf6f73ae4551acb1b082c26cb983e392

--- device.mk
+++ device.mk
@@ -318,6 +318,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/power/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
 
+# Protobuf
+PRODUCT_PACKAGES += \
+    libprotobuf-cpp-full-3.9.1-vendorcompat
+
 # QTI
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect.vendor
