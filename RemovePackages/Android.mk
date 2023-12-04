b3e8b3125288cfdd89bff65a40c50db7d0c81b60
S A U R A V <dwarmachine24@gmail.com>
2023-12-04 17:11:27 +0530

    Update Android.mk

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -4,18 +4,48 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := AmbientSensePrebuilt AppDirectedSMSService arcore
-LOCAL_OVERRIDES_PACKAGES += CarrierSetup Chrome Chrome-Stub ConnMO ConnMetrics
-LOCAL_OVERRIDES_PACKAGES += DCMO DMService
-LOCAL_OVERRIDES_PACKAGES += MaestroPrebuilt Maps MyVerizonServices NovaBugReportWrapper Ornament OemDmTrigger OBDM_Permissions
-LOCAL_OVERRIDES_PACKAGES += PrebuiltGmail GoogleCalenderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += RecorderPrebuilt
-LOCAL_OVERRIDES_PACKAGES += SCONE ScribePrebuilt Showcase SoundAmplifierPrebuilt SprintDM SprintHM
-LOCAL_OVERRIDES_PACKAGES += USCCDM Photos
-LOCAL_OVERRIDES_PACKAGES += Videos VZWAPNLib VzwOmaTrigger obdm_stub
-LOCAL_OVERRIDES_PACKAGES += WallpapersBReel2020
-LOCAL_OVERRIDES_PACKAGES += YouTube YouTubeMusicPrebuilt GoogleCamera
+LOCAL_OVERRIDES_PACKAGES := \
+          arcore \
+          AmbientSensePrebuilt \
+          AppDirectedSMSService \
+          ConnMO \
+          CalendarGooglePrebuilt \
+          Drive \
+          DCMO \
+          DevicePolicyPrebuilt \
+          DMService \
+          GCS \
+          GoogleCamera \
+          MaestroPrebuilt \
+          Maps \
+          MicropaperPrebuilt \
+          MyVerizonServices \
+          OBDM_Permissions \
+          obdm_stub \
+          PrebuiltGmail \
+          PixelWallpapers2021 \
+          PixelWallpapers2020 \
+          RecorderPrebuilt \
+          SafetyHubPrebuilt \
+          SCONE \
+          ScribePrebuilt \
+          Showcase \
+          Snap \
+          SoundAmplifierPrebuilt \
+          SprintDM \
+          SprintHM \
+          TurboPrebuilt \
+          Tycho \
+          TipsPrebuilt \
+          talkback \
+          USCCDM \
+          VZWAPNLib \
+          VzwOmaTrigger \
+          WallpapersBReel2020 \
+          YouTube \
+          YouTubeMusicPrebuilt 
+
 LOCAL_UNINSTALLABLE_MODULE := true
-LOCAL_CERTIFICATE := PRESIGNED
+LOCAL_CERTIFICATE := platform
 LOCAL_SRC_FILES := /dev/null
 include $(BUILD_PREBUILT)
